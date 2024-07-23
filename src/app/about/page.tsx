af070bda83ac47454fc0cf7ae36ac0073b78aed1
Richard Essuman <ressuman001@gmail.com>
2024-07-23 12:19:59 +0000

    chore: Update global CSS and add About page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,26 @@
+export default function About() {
+  return (
+    <main>
+      <h2>About</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit molestias
+        eum odio vel aspernatur cum molestiae consequuntur natus minus. Fugiat
+        nisi sint dolores obcaecati quos cumque itaque ratione error amet.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis,
+        expedita. Asperiores esse vero voluptate odio cum delectus quisquam est
+        dolor. Commodi inventore maxime delectus molestias blanditiis iste. Quae
+        animi ab voluptatum voluptate recusandae, eveniet accusamus explicabo
+        alias fugiat voluptatibus aperiam adipisci tempore dolorem quis
+        veritatis hic iusto laborum. Nam nemo iusto eveniet blanditiis fugiat
+        quis consequatur asperiores possimus sequi hic ducimus suscipit
+        assumenda repudiandae accusantium, non similique! Sed mollitia suscipit
+        quis accusamus vel laborum dolores, quibusdam natus, illum ea, sequi
+        quam voluptatibus temporibus labore consequuntur ducimus atque esse aut
+        minus quas officiis debitis? Sequi vitae eveniet illo velit quaerat
+        nobis!
+      </p>
+    </main>
+  );
+}
